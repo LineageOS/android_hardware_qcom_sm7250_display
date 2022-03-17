b38550b67285a46961d4d4f6d7b7d8d5f4632b5a
Bob Badour <bbadour@google.com>
2022-03-17 08:08:16 -0700

    [LSC] Add LOCAL_LICENSE_KINDS to hardware/qcom/sm7250/display
    Added SPDX-license-identifier-BSD to:
      init/Android.mk
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m all
    Change-Id: Iad4bdca095133ea7063c3f5010961c5cd9b0cd9d

--- init/Android.mk
+++ init/Android.mk
@@ -6,4 +6,6 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.qti.display_boot.sh
 LOCAL_INIT_RC      := init.qti.display_boot.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+LOCAL_LICENSE_KINDS := SPDX-license-identifier-BSD
+LOCAL_LICENSE_CONDITIONS := notice
 include $(BUILD_PREBUILT)
