d3fae6913731a30c135a6cd1fab803913da06998
Anand S <anandzzz360@gmail.com>
2026-01-07 19:59:39 +0530

    display: Fix duplicate property overrides
    * In redbull tree, we're using Natural to be the default color mode.
    
    Change-Id: Ib9dd2bc8cf09b2665508de4a7097603f7c2a0f80

--- config/display-product.mk
+++ config/display-product.mk
@@ -21,7 +21,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.sf.color_saturation=1.0 \
-    persist.sys.sf.color_mode=9 \
+    persist.sys.sf.color_mode=0 \
     debug.sf.hw=0 \
     debug.egl.hw=0 \
     debug.mdpcomp.logs=0 \
